7f42e5d9fa821595b78cd50abf87b42a6d387db4
Abdul Muksith Rizvi <57699415+abdulmuksith3@users.noreply.github.com>
2025-04-10 01:34:50 +0300

    Revert "Merge pull request #1 from abdulmuksith3/vercel"
    This reverts commit f25b6f0b835654b2c51efe7a340ef0fd78b494e5, reversing
    changes made to 7d2fd0ca873461aefa056f8b8b0efada4f559e7e.

--- api/index.py
+++
@@ -1,7 +0,0 @@
-# api/index.py
-
-from vercel_wsgi import handle_request
-from app import app  # imports your existing app
-
-def handler(event, context):
-    return handle_request(app, event, context)
